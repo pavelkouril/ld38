ec17001f0eee92d998ecc3fc5aceb0d54ebbf202
Pavel Kouřil <pk@pavelkouril.cz>
2017-04-22 20:05:08 +0200

    teleport stops the player

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Teleport : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("You win, you fool.");
+            yield return new WaitForSeconds(0.5f);
+            foreach (var c in GetComponents<Collider>())
+            {
+                c.isTrigger = false;
+            }
         }
     }
 }
