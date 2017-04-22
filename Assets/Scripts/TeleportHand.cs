22d30e3bc2163f16e0f3d1c897a5411b6310a2df
David Kadlec <kadlec.david@gmail.com>
2017-04-23 01:03:05 +0200

    Teleport hands

--- Assets/Scripts/TeleportHand.cs
+++ Assets/Scripts/TeleportHand.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TeleportHand : MonoBehaviour
 {
     public bool IsAnimating = false;
+    public float Force;
+    public float Acc;
 
     // Update is called once per frame
     void Update()
@@ -13,6 +15,17 @@ public class TeleportHand : MonoBehaviour
         {
             Debug.Log("IsAnimating");
 
+            StartCoroutine(Animate());
+        }
+    }
+
+    private IEnumerator Animate()
+    {
+        while (true)
+        {
+            transform.Rotate(0, Mathf.Max(0.001f, Force / 1000 + Acc / 100), 0);
+
+            yield return new WaitForSeconds(0.05f);
         }
     }
 }
