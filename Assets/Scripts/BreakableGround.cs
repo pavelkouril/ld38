9be06345ebfadb9a0e5b66f220bd2d9efd61ee53
Pavel Kouřil <pk@pavelkouril.cz>
2017-04-22 16:33:24 +0200

    added deleteing of tiles, made prototype smaller

---
+++ Assets/Scripts/BreakableGround.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RUF
+{
+    public class BreakableGround : MonoBehaviour
+    {
+        private void OnTriggerExit(Collider other)
+        {
+            Debug.Log("collide");
+            if (other.CompareTag("Destroyer"))
+            {
+                Debug.Log("collide");
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RUF
+{
+    public class ObjectDestroyer : MonoBehaviour
+    {
+        private new BoxCollider collider;
+
+        private void Awake()
+        {
+            collider = GetComponent<BoxCollider>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(MakeColliderSmaller());
+        }
+
+        private IEnumerator MakeColliderSmaller()
+        {
+            while (true)
+            {
+                collider.size = collider.size - new Vector3(1, 1, 1);
+                yield return new WaitForSeconds(2);
+            }
+        }
+    }
+}
