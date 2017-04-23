7869efbdb2ee4c5889add184e42ae9e06aa09170
David Kadlec <kadlec.david@gmail.com>
2017-04-23 16:51:02 +0200

    Gate sound

---
+++ Assets/Scripts/ForceGateSound.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceGateSound : MonoBehaviour {
+
+    private bool IsPlaying = false;
+    private AudioSource Audio;
+
+    private void Awake()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            IsPlaying = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            IsPlaying = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (IsPlaying)
+        {
+            if (!Audio.isPlaying)
+            {
+                Audio.Play();
+            }
+
+            if (Audio.volume < 0.15)
+            {
+                Audio.volume += Time.deltaTime * 1f / 10f;
+            }
+        }
+        else
+        {
+            Audio.volume -= Time.deltaTime * 1f/10f;
+
+            if (Audio.volume <= 0)
+            {
+                Audio.Stop();
+            }
+        }
+    }
+}
