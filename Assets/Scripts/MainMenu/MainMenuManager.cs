bf5aad779acb5617d8ba0e923ceb3ef9c6a181b0
Pavel Kouřil <pk@pavelkouril.cz>
2017-04-23 02:45:19 +0200

    basic main menu

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RUF.MainMenu
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        public void StartButtonPressed()
+        {
+            SceneManager.LoadScene("00-Tutorial");
+        }
+
+        public void QuitButtonPressed()
+        {
+            Application.Quit();
+        }
+    }
+}
