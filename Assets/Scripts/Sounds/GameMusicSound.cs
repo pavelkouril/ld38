bf95947897a6e766fc93731617cf7942a23bb698
Pavel Kouřil <pk@pavelkouril.cz>
2017-04-23 17:30:02 +0200

    bgm is always played

---
+++ Assets/Scripts/Sounds/GameMusicSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RUF
+{
+    public class GameMusicSound : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
