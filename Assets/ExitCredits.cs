a523a20caf6bdcc032556de0b3b8ee35d8be0406
Pavel Kouřil <pk@pavelkouril.cz>
2017-04-24 00:42:02 +0200

    fixed levels

---
+++ Assets/ExitCredits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitCredits : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonUp("Submit"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
