df701e8dfcfaad32bd64f003c4eb369cc06477bc
Vilem Otte <vilem.otte@post.cz>
2017-04-25 02:44:16 +0200

    Camera path

---
+++ Assets/CameraPath.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPath : MonoBehaviour {
+
+    public GameObject waypoint0;
+    public GameObject waypoint1;
+    public GameObject waypoint2;
+    public GameObject waypoint3;
+    public GameObject waypoint4;
+    public float speed = 1.0f;
+    private Transform[] waypoint;
+    private int current;
+    private int next;
+
+    // Use this for initialization
+    void Start () {
+        waypoint = new Transform[5];
+        waypoint[0] = waypoint0.transform;
+        waypoint[1] = waypoint1.transform;
+        waypoint[2] = waypoint2.transform;
+        waypoint[3] = waypoint3.transform;
+        waypoint[4] = waypoint4.transform;
+        current = 0;
+        next = 1;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 a = transform.position;
+        Vector3 b = waypoint[current].position;
+
+        if (Vector3.Distance(a, b) > 0.05f)
+        {
+            Vector3 dir = b - a;
+            dir.Normalize();
+            transform.position = transform.position + dir * Time.deltaTime * speed;
+        }
+
+        Quaternion qa = transform.rotation;
+        Quaternion qb = Quaternion.LookRotation(waypoint[next].position - transform.position);
+        transform.rotation = Quaternion.Slerp(qa, qb, Time.deltaTime);
+    }
+
+    public void Next()
+    {
+        if (next != 4)
+        {
+            current++;
+            next++;
+        }
+    }
+}
---
+++ Assets/CameraPathWaypoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPathWaypoint : MonoBehaviour {
+
+    public GameObject cameraCollision;
+    private CameraPath cameraPath;
+    private bool active;
+
+	// Use this for initialization
+	void Start () {
+        cameraPath = cameraCollision.GetComponent<CameraPath>();
+        active = true;
+    }
+
+    void Update()
+    {
+        if (active)
+        {
+            if (Vector3.Distance(transform.position, cameraCollision.transform.position) < 0.1f)
+            {
+                cameraPath.Next();
+                active = false;
+            }
+        }
+    }
+}
